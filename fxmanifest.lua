f28d898c2be505db677a5ceaab95f60eb1506e6d
Yuri de Oliveira <161542848+YuriOliveira1@users.noreply.github.com>
2024-10-29 19:24:50 -0700

    First Commit

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Yuri de Oliveira'
+description 'Atm Robbery For QBX'
+version '1.0.0'
+
+-- What to run
+client_scripts {
+    'client.lua',
+}
+server_script 'server.lua'
